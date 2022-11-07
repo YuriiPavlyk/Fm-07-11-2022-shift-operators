48a6bedffbc7cfebdf72db74467559679139d6b1
yurii <pavlykyurii15@gmail.ru>
2022-11-07 14:35:52 +0200

    Arithmetic operations

---
+++ index.js
@@ -0,0 +1,19 @@
+let number1 = 45;
+let number2 = 12;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+
+let number = 20;
+number = number + 6;
+number = number + 1;
+number = number / 2;
+number = number - 3;
+number = number - 1;
+number = number % 4;
+number = number + "qwerty";
+console.log(number)
+
