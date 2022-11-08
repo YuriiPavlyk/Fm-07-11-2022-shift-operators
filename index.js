b16f51b2ddca539f736628e0e7d06cd9385fdb5f
YuriiPavlyk <116871825+YuriiPavlyk@users.noreply.github.com>
2022-11-08 09:12:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ console.log(number1 / number2);
 console.log(number1 % number2);
 
 let number = 20;
-number = number + 6;
-number = number + 1;
-number = number / 2;
-number = number - 3;
-number = number - 1;
-number = number % 4;
-number = number + "qwerty";
+number+=6;
+number+=1;
+number/=2;
+number-=3;
+number-=1;
+number%=4;
+number+="qwerty";
 console.log(number)
 
